62129577e674ad5964c7c7a1d98ba46349144581
TukS <123561095+youbiii@users.noreply.github.com>
2025-10-08 23:38:46 +0700

    update
    1

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -1,15 +1,6 @@
 @extends('layout.app')
 @section('content')
 <!-- Page Heading -->
-                    <div class="d-sm-flex align-items-center justify-content-between mb-4">
-
-                    @foreach ($fakultas as $item )
-                     <h1 class="h3 mb-0 text-gray-800">{{ $item->nama_fakultas }}</h1>
-                    @endforeach
-                       
-                        <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i
-                                class="fas fa-download fa-sm text-white-50"></i> Generate Report</a>
-                    </div>
 
                     <!-- Content Row -->
                     <div class="row">
